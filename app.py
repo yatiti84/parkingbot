9174dc4baa6f2e5bece8eada790c3e83bb5b913e
susu <yatiti84@gmail.com>
2023-12-23 05:40:05 +0800

    fix: receive request body as raw data to make validation encoding

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def health_test():
 
 @app.route('/receiver', methods=['GET', 'POST'])
 def receiver():
-    print(flask.request.args.get('destination'))
-    print(flask.request.args.get('events'))
+    print(flask.request.data)
     print(flask.request.get_json())
     print(flask.request.headers)
     webhook_manager = webhook.Webhook(flask.request)
