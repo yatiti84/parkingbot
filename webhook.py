783f9120bb7af88c1d2f4fd5db7480c59d60d452
susu <yatiti84@gmail.com>
2023-12-23 13:12:43 +0800

    fix: fix typo channel_access_token

--- webhook.py
+++ webhook.py
@@ -29,7 +29,8 @@ class Webhook():
             x_line_signature = self.request_header.get(
                 'X-Line-Signature')
         # Compare  request header and the signature
-        print(f"signatureValidation is{x_line_signature.encode('utf-8') == signature}")
+        print(
+            f"signatureValidation is{x_line_signature.encode('utf-8') == signature}")
         return x_line_signature.encode('utf-8') == signature
 
     def requestBodyCheck(self):
@@ -37,7 +38,11 @@ class Webhook():
         # events = self.request_body.get('events')
         return
 
-    def sendReply(self, reply_token:str)-> None :
+    def sendReply(self, reply_token: str) -> None:
+        headers = {
+            'Content-Type': 'application/json', 
+            'Authorization': f'Bearer {self.channel_access_token}'
+            }
         reply_data = json.dumps({
             "replyToken": reply_token,
             "messages": [
@@ -51,9 +56,10 @@ class Webhook():
                 }
             ]
         })
-        raw_payment_resp = requests.post(url=self.line_reply_url, headers={
-                                         'Content-Type': 'application/json', 'Authorization': f'Bearer {{{self.channel_secret}}}'}, data=reply_data)
+        print(headers)
+        raw_payment_resp = requests.post(
+            url=self.line_reply_url, headers=headers, data=reply_data)
         print(raw_payment_resp.status_code)
         print(raw_payment_resp.text)
-        
-        return 
+
+        return
