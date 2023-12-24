f28b67b64e1484f6958a11f5c6b568ae51dcd3ce
susu <yatiti84@gmail.com>
2023-12-24 17:20:41 +0800

    feat: implement invoice donation

--- parkingPayment.py
+++ parkingPayment.py
@@ -12,6 +12,7 @@ class ParkingPayment():
         self.carrier_id = os.environ.get('carrierId')
         self.payment_type = os.environ.get('payment_type')
         self.carrier_type = os.environ.get('carrier_type')
+        self.npoban = os.environ.get('NPOBAN')
 
     def callParkingApi(self) -> str:
 
@@ -24,10 +25,12 @@ class ParkingPayment():
                            "PaymentType": self.payment_type,
                            "CarrierType": self.carrier_type,
                            "CarrierId": self.carrier_id,
+                           "NPOBAN": self.npoban,
                            "redirect_url": self.redirect_url}
 
         resp = requests.post(self.check_url, data=payment_content)
         print(resp.content.decode('utf-8'))
+        # {"message":"交易失敗，載具系統連線異常"}
         # resp_content = json.loads(resp.content)
         # payment_url = str(payment_url)
 
