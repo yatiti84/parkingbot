8e43c5e9e259e1cae976ab06eba6af22c5f1bb00
susu <yatiti84@gmail.com>
2023-12-24 22:48:23 +0800

    fix: msg check

--- app.py
+++ app.py
@@ -13,15 +13,14 @@ def health_test():
 
 @app.route('/receiver', methods=['GET', 'POST'])
 def receiver():
-    print(flask.request.data)
     print(flask.request.get_json())
-    print(flask.request.headers)
     webhook_manager = webhook.Webhook(flask.request)
     if webhook_manager.signatureValidation() and webhook_manager.requestEventsCheck():
         for raw_event in webhook_manager.request_events:
-            if raw_event['type'] == 'message':
+            if raw_event['type'] == 'message' and raw_event['message']['type'] == 'text':
                 request_event = event.MessageEvent(raw_event)
                 action = request_event.parseText(request_event.message_text)
+                print(action)
                 if action['is_start']:
                     print("start to send reply")
                     payment_manager = parkingPayment.ParkingPayment(
